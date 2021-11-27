f8d178453a5e2ea721f8920cdd424a6956af850f
Michael Kamprath <michael@kamprath.net>
2021-11-26 18:45:38 -0800

    started work to add HL register to microcode

---
+++ stack-pointer/examples/index_register_example.asm
@@ -0,0 +1,22 @@
+; Demostration of HL Register Usage
+;
+; This is a simple demostration of usage of the indiex register on the PUTEY-1 (BETA-2).
+; It will increment the HL register and then copy the value to the MAR peridocically.
+; Ideally the LEDs on HL and MAR will allow you to see what is happening.
+
+.org 0
+init:
+    rsp
+.restart:
+    mov2 hl,0
+    mov i,0
+.iloop:
+    mov2 mar, hl
+    inc i
+    jc .restart
+    mov j, i
+.hlloop:
+    inc hl
+    dec j
+    jz .iloop
+    jmp .hlloop
