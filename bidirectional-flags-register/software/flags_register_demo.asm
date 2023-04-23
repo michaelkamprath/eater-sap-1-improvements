ddb44a67d2a80f681760d4e36c6515a14bdbe928
Michael Kamprath <michael@kamprath.net>
2023-04-23 16:34:48 -0700

    made flags register writable

---
+++ bidirectional-flags-register/software/flags_register_demo.asm
@@ -0,0 +1,134 @@
+#require "putey-1-beta >= 0.5.0"
+#include "kernel.asm"
+
+BUFFER_SIZE = 21
+DELAY_COUNTER = $1000
+
+; the goal of this program is to demonstrate the F (flags) register being able to be
+; restored. It will do these steps:
+;   1. clear flags register, and print its current content
+;   2. do some math that causes a carry, and then print flags register conent
+;   3. push flags register to stack, then clear flags egister contents and print that.
+;   4. pop flag register value from after step 2 and print that.
+;   5. set flag regiser so that only the equal flag (EF) is set, and demonstrate branching off that.
+;   6. Success!
+
+.org 0 "application_code"
+start:
+.step1:
+    push2 step1_message
+    call lcd_print_line_cstr
+    pop2
+    mov f,0
+    call print_flag_bits
+    push2 DELAY_COUNTER
+    call delay16
+    pop2
+
+.step2:
+    push2 step2_message
+    call lcd_print_line_cstr
+    pop2
+    mov a,$ee
+    add $42
+    call print_flag_bits
+    push2 DELAY_COUNTER
+    call delay16
+    pop2
+
+.step3:
+    push f
+    push2 step1_message
+    call lcd_print_line_cstr
+    pop2
+    mov f,0
+    call print_flag_bits
+    push2 DELAY_COUNTER
+    call delay16
+    pop2
+
+.step4:
+    push2 step4_message
+    call lcd_print_line_cstr
+    pop2
+    pop f
+    call print_flag_bits
+    push2 DELAY_COUNTER
+    call delay16
+    pop2
+
+.step5:
+    push2 step5_message
+    call lcd_print_line_cstr
+    pop2
+    mov f,EQUALS_FLAG
+    call print_flag_bits
+    push2 DELAY_COUNTER
+    call delay16
+    pop2
+    mov f,EQUALS_FLAG
+    je .branch_on_equals:
+    push2 step5_not_equals_message
+    jmp .end
+.branch_on_equals:
+    push2 step5_equals_message:
+.end:
+    call lcd_print_line_cstr
+    pop2
+    hlt
+
+print_flag_bits:
+    ; save a and i
+    push2 hl
+    push a
+    push i
+    push f                  ; save flag value to be printed
+    push2 str_current_flags_prefix
+    push2 str_buffer1
+    call cstr_copy
+    call cstr_len8          ; place str length into A
+    pop2
+    pop2
+    pop i                   ; pop flag bits into i
+    push a                  ; A has where the binary str should start
+    push2 str_buffer1
+    push i                  ; push flag state in I onto stack
+    call uint8_to_binary_cstr
+    pop i                   ; restore F state to I
+    pop2
+    pop
+    push i                  ; save F state into I again
+    push2 uint8_to_binary_cstr
+    call lcd_print_line_cstr
+    pop2
+.done:
+    ; restore states
+    pop f
+    pop i
+    pop a
+    pop2 hl
+    ret
+
+
+step1_message:
+    .cstr "Clearing flag bits"
+step2_message:
+    .cstr "Adding $EE+$42.."
+step4_message:
+    .cstr "Restoring flag bits"
+step5_message:
+    .cstr "Setting EQUAL flag"
+step5_equals_message:
+    .cstr "Branched on EQUAL=1"
+step5_not_equals_message:
+    .cstr "Branched on EQUAL=0"
+
+str_current_flags_prefix:
+    .cstr "Flag Bits: "
+
+
+.memzone application_ram
+str_buffer1:
+    .zero BUFFER_SIZE
+str_buffer2:
+    .zero BUFFER_SIZE
---
+++ bidirectional-flags-register/software/kernel/utilitylib.asm
@@ -0,0 +1,94 @@
+#require "putey-1-beta >= 0.5.0"
+
+;
+; General System Utilities
+;
+.memzone system_code
+
+; delay8
+;   delays while counting down an 8-bit counter.
+;
+;   arguments:
+;       sp+2    - An 8-bit value to count out
+;
+;   returns:
+;       nothing
+;
+;   registers used:
+;       a
+;       i
+;
+delay8:
+    mov i, [sp+2]
+    cmp i,0
+    je .end
+.loop:
+    dec i
+    jnz .loop
+.end:
+    ret
+
+
+; delay16
+;   delays while counting down an 16-bit counter.
+;
+;   arguments:
+;       sp+2    - An 16-bit value to count out (2 bytes)
+;
+;   returns:
+;       nothing
+;
+;   registers used:
+;       hl
+delay16:
+    mov2 hl, [sp+2]
+.loop:
+    dec hl
+    jnz .loop
+.end:
+    ret
+
+; cmp16
+;   Compares two 16-bit values, and sets OF (left > right) or EF (left == right) flags
+;
+;   Arguments
+;       sp+2 : left value (2 bytes)
+;       sp+4 : right value (2 bytes)
+;
+;   Returns
+;       Nothing
+;
+; Registers used:
+;    a
+cmp16:
+    ; first check high bytes
+    cmp [sp+3],[sp+5]
+    jne .done
+    cmp [sp+2],[sp+4]
+.done:
+    ret
+
+
+; cmp32
+;   Compares two 32-bit values, and sets OF (left > right) or EF (left == right) flags
+;
+;   Arguments
+;       sp+2 : left value (4 bytes)
+;       sp+6 : right value (4 bytes)
+;
+;   Returns
+;       Nothing
+;
+; Registers used:
+;    a
+cmp32:
+    ; first check high bytes
+    cmp [sp+5],[sp+9]
+    jne .done
+    cmp [sp+4],[sp+8]
+    jne .done
+    cmp [sp+3],[sp+7]
+    jne .done
+    cmp [sp+2],[sp+6]
+.done:
+    ret
